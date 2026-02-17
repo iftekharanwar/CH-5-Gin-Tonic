b614e3c5d67e020a5e8ebaa712480a86e5c86ad2
Iftekhar Anwar <116110091+iftekharanwar@users.noreply.github.com>
2026-02-17 23:45:27 +0100

    Initial Commit

---
+++ neura/neuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  neuraApp.swift
+//  neura
+//
+//  Created by Iftekhar Anwar on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct neuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
